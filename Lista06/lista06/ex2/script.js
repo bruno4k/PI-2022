ce9af0d07c42d8f060b6a701361053dfe18327a3
bruno4k <60243548+bruno4k@users.noreply.github.com>
2022-07-01 19:50:02 -0300

    Delete Lista06/lista06 directory

--- Lista06/lista06/ex2/script.js
+++
@@ -1,8 +0,0 @@
-var lista = document.getElementById("lista-vazia");
-document.getElementById("add-content").addEventListener("click", () => {
-    let li = document.createElement('li');
-    let element = document.getElementById("content").value;
-    li.appendChild(document.createTextNode(element));
-    lista.appendChild(li);
-    document.getElementById("content").value = '';
-})
